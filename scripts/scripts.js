b0e4364b6beb10b7c052d21ad3a68a7a01456c87
junaid Anwar <Junaid4567@gmail.com>
2016-01-10 19:32:14 +0500

    img.src will give error so use ng-src for images sources

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,11 @@
-var myApp = angular
-	.module("myModule", []) //creating module
+var myApp = angular.module("myModule", []) //creating module
 	.controller("myController", function ($scope) {  //Creating and registering controller
-	var employee = {
-		firstName : "David",
-		lastName  : "Hastings",
-		gender	  : "male",
-	};
-	$scope.employee = employee;
+	var countryInfo = {
+		name 	: "Pakistan",
+		capital	: "Islamabad",
+		flag	: "images/flag-pk.GIF",
+	}
+	$scope.countryInfo = countryInfo;
 });
 // 	var myApp = angular.module("myModule", []) //creating module
 // 	.controller("myController", function ($scope) {  //Creating and registering controller
