a49a4d74ce9d20d36a0f301b0b17bc8ae64fdf36
junaid Anwar <Junaid4567@gmail.com>
2016-01-10 19:13:19 +0500

    finished learning basics create module, create controller, register controller with the module

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,20 @@
+var myApp = angular
+	.module("myModule", []) //creating module
+	.controller("myController", function ($scope) {  //Creating and registering controller
+	var employee = {
+		firstName : "David",
+		lastName  : "Hastings",
+		gender	  : "male",
+	};
+	$scope.employee = employee;
+});
+// 	var myApp = angular.module("myModule", []) //creating module
+// 	.controller("myController", function ($scope) {  //Creating and registering controller
+// 	var employee = {
+// 		firstName : "David",
+// 		lastName  : "Hastings",
+// 		gender	  : "male",
+// 	};
+// 	$scope.employee = employee;
+// });
 
-var myApp = angular.module("myModule", []);
-
-var myController = function ($scope) {
-	$scope.message = "Angular Tutorial";
-};
-myApp.controller("myController", myController);
