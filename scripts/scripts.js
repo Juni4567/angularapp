83499c56a1ef9eaef60519cc0a448d4abf7ae629
junaid Anwar <Junaid4567@gmail.com>
2016-01-10 21:08:11 +0500

    successfully completed ng-repeat with nesting

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,37 @@
+var countryApp = angular.module("countryApp", []) //creating module
+	.controller("countryController", function ($scope) {  //Creating and registering controller
+		var countries	= [
+		{
+			name : "United Kingdom",
+			cities : [
+				{ name : "London"},
+				{ name : "Manchester"},
+				{ name : "Birmingham"}
+			]
+		},
+		{
+			name : "United States",
+			cities : [
+				{ name : "Los Angeles"},
+				{ name : "New York"},
+				{ name : "Washington D.C"}
+			]
+		},
+		{
+			name : "Pakistan",
+			cities : [
+				{ name : "Rawalpindi"},
+				{ name : "Islamabad"},
+				{ name : "Karachi"}
+			]
+		},
+		];
+		$scope.countries = countries; // name that will be used inside the scope of the controller
+	});
+
+
+
+//trying to see if this code interfare with the one above because I renamed the controller and changing the name in the markup as well
 var myApp = angular.module("myModule", []) //creating module
 	.controller("myController", function ($scope) {  //Creating and registering controller
 		var employees = [
