024d245599d97a641d08763a2b10ac321b8ed72d
junaid Anwar <Junaid4567@gmail.com>
2016-01-10 20:44:49 +0500

    learnt how to display data inside table and ng-repeat

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,15 @@
 var myApp = angular.module("myModule", []) //creating module
 	.controller("myController", function ($scope) {  //Creating and registering controller
-	var countryInfo = {
-		name 	: "Pakistan",
-		capital	: "Islamabad",
-		flag	: "images/flag-pk.GIF",
-	}
-	$scope.countryInfo = countryInfo;
-});
+		var employees = [
+		{ firstName : "Junaid", lastName : "Anwar", gender : "Male", salary : 10000},
+		{ firstName : "Jawad", lastName : "Anwar", gender : "Male", salary : 2000},
+		{ firstName : "Hammad", lastName : "Anwar", gender : "Male", salary : 5000},
+		{ firstName : "Junaid", lastName : "Anwar", gender : "Male", salary : 10000},
+		{ firstName : "Junaid", lastName : "Anwar", gender : "Male", salary : 10000}
+		];
+
+		$scope.employees = employees; // name that will be used inside the scope of the controller
+	});
 // 	var myApp = angular.module("myModule", []) //creating module
 // 	.controller("myController", function ($scope) {  //Creating and registering controller
 // 	var employee = {
